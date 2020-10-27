9176446e22c8960067cfc1246ec6fc5e6eb39ee7
Ian Chen <ian-@gmx.com>
2020-10-27 19:57:09 +0800

    override_dev env var for local testing

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ const scopes = process.env.SCOPES
 const clientId = process.env.CLIENT_ID
 const hostname = process.env.HOSTNAME
 const PORT = process.env.PORT || 10000
+const overrideDev = process.env.OVERRIDE_DEV
 
 const { fetchToken, fetchUserInfo } = require('./scripts/callback')
 
 const BASE_URLS = {
   prod: 'https://api.id.gov.sg',
   stag: 'https://api-staging.id.gov.sg',
-  dev: 'https://api-dev.id.gov.sg',
+  dev: overrideDev || 'https://api-dev.id.gov.sg',
 }
 
 const express = require('express')
