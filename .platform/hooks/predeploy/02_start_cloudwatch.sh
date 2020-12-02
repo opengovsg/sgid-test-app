0791614ae7e41df4fee6c06e0d865dadd63b5d20
Ian Chen <ian-@gmx.com>
2020-12-02 21:30:28 +0800

    Cloudwatch agent (#36)
    * test platform hook
    
    * set perms for executables
    
    * fix: wildcard issue
    
    * fix: stray comma
    
    * fix: try again
    
    * fix: do not append
    
    * fix: escape dollar sign
    
    * fix: move to predeploy
    
    * fix: run as root
    
    * fix: add node engine

---
+++ .platform/hooks/predeploy/02_start_cloudwatch.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# create cloudwatch agent config file
+echo "running 02_start_cloudwatch hook"
+cat <<EOT > /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
+{
+  "agent": {
+    "metrics_collection_interval": 30,
+    "run_as_user": "root"
+  },
+  "metrics": {
+    "append_dimensions": {
+      "AutoScalingGroupName": "\${aws:AutoScalingGroupName}",
+      "ImageId": "\${aws:ImageId}",
+      "InstanceId": "\${aws:InstanceId}",
+      "InstanceType": "\${aws:InstanceType}"
+    },
+    "metrics_collected": {
+      "disk": {
+        "measurement": ["used_percent"],
+        "metrics_collection_interval": 60,
+        "resources": ["*"],
+        "ignore_file_system_types": ["sysfs", "devtmpfs"]
+      },
+      "mem": {
+        "measurement": ["mem_used_percent"],
+        "metrics_collection_interval": 60
+      },
+      "swap": {
+        "measurement": ["swap_used_percent"],
+        "metrics_collection_interval": 60
+      }
+    }
+  }
+}
+EOT
+
+/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a append-config -m ec2 -s -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
