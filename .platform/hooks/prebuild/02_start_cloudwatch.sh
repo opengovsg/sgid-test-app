25d1f7b069b223f1c41d6061e23b15d30e9b8aa6
Ian Chen <ian-@gmx.com>
2020-12-02 19:01:03 +0800

    fix: try again

--- .platform/hooks/prebuild/02_start_cloudwatch.sh
+++ .platform/hooks/prebuild/02_start_cloudwatch.sh
@@ -3,40 +3,32 @@
 echo "running 02_start_cloudwatch hook"
 cat <<EOT >> /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
 {
-	"agent": {
-		"metrics_collection_interval": 60,
-		"run_as_user": "cwagent"
-	},
-	"metrics": {
-		"append_dimensions": {
-			"InstanceId": "${aws:InstanceId}",
-			"InstanceType": "${aws:InstanceType}"
-		},
-		"metrics_collected": {
-			"disk": {
-				"measurement": [
-					"used_percent"
-				],
-				"metrics_collection_interval": 60,
-				"resources": [
-					"*"
-				]
-			},
-			"mem": {
-				"measurement": [
-					"mem_used_percent"
-				],
-				"metrics_collection_interval": 60
-			},
-			"swap": {
-				"measurement": [
-					"swap_used_percent"
-				],
-				"metrics_collection_interval": 60
-			}
-		}
-	}
+  "agent": {
+    "metrics_collection_interval": 30,
+    "run_as_user": "cwagent"
+  },
+  "metrics": {
+    "append_dimensions": {
+      "AutoScalingGroupName": "${aws:AutoScalingGroupName}",
+      "ImageId": "${aws:ImageId}",
+      "InstanceId": "${aws:InstanceId}",
+      "InstanceType": "${aws:InstanceType}"
+    },
+    "metrics_collected": {
+      "disk": {
+        "measurement": ["used_percent"],
+        "metrics_collection_interval": 30,
+        "resources": ["*"],
+        "ignore_file_system_types": ["sysfs", "devtmpfs"]
+      },
+      "mem": {
+        "measurement": ["mem_used_percent"],
+        "metrics_collection_interval": 30
+      }
+    }
+  }
 }
+
 EOT
 
 /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a append-config -m ec2 -s -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
