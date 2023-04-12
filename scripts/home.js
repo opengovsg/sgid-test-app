89698e7f4a1a1b7c9acbfd5655424ecc22ae9e05
Prawira Genestonlia <prawira.96@gmail.com>
2023-04-12 13:07:38 +0800

    feat: improvement on using SgidClient

--- scripts/home.js
+++ scripts/home.js
@@ -1,9 +1,5 @@
-const scopes = process.env.SCOPES
-const clientId = process.env.CLIENT_ID
-const hostname = process.env.HOSTNAME
-
 const config = require('../lib/config')
-
+const {sgidClient, scopes, randomnonce} = require('../lib/sgid-client-singleton')
 /**
  * Main controller function to generate the home page
  *
@@ -12,10 +8,8 @@ const config = require('../lib/config')
  */
 function index(_req, res) {
   const authUrl = {}
-  for (const [env, baseurl] of Object.entries(config.baseUrls)) {
-    authUrl[env] = baseurl
-      ? `${baseurl}/v1/oauth/authorize?response_type=code&client_id=${clientId}&scope=${scopes}&redirect_uri=${hostname}/callback&nonce=randomnonce&state=${env}`
-      : undefined
+  for (const [env, ] of Object.entries(config.baseUrls)) {
+	authUrl[env] = sgidClient[env].authorizationUrl(env,scopes,randomnonce).url
   }
 
   res.render('index', { authUrl })
