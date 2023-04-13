f018e27ed052f0a1187576e2ad1e7defbed2ee00
Prawira Genestonlia <prawira.96@gmail.com>
2023-04-13 17:28:07 +0800

    feat: refactor current SSR into API based to enable mobile sgID flow

--- scripts/callback.js
+++ scripts/callback.js
@@ -1,4 +1,5 @@
-const {sgidClient, randomnonce} = require('../lib/sgid-client-singleton')
+const { sgidClient, randomnonce } = require('../lib/sgid-client-singleton')
+const { formatData } = require('../lib/utils')
 
 /**
  * Main controller function to generate the callback page
@@ -7,49 +8,19 @@ const {sgidClient, randomnonce} = require('../lib/sgid-client-singleton')
  * @param {*} res
  */
 async function index(req, res) {
-  try {
-    const { code, state } = req.query
-	const { accessToken } = await sgidClient[state].callback(code,randomnonce)
-    const { sub, data } = await sgidClient[state].userinfo(accessToken)
-    const formattedData = formatData(data)
+	try {
+		const { code, state } = req.query
+		const { accessToken } = await sgidClient[state].callback(code, randomnonce)
+		const { sub, data } = await sgidClient[state].userinfo(accessToken)
+		const formattedData = formatData(data)
 
-    res.render('callback', {
-      data: [['sgID', sub], ...formattedData],
-    })
-  } catch (error) {
-    console.error(error)
-    res.status(500).render('error', { error })
-  }
-}
-
-/**
- * Formats the data into an array of arrays,
- * specifically for the display on the frontend
- *
- * @param {object} result
- * @returns {array}
- */
-function formatData(result) {
-  const formattedResult = []
-
-  for (const [key, value] of Object.entries(result)) {
-    formattedResult.push([prettifyKey(key), value])
-  }
-
-  return formattedResult
-}
-
-/**
- * Converts a key string from dot-delimited into uppercase
- * for frontend display
- *
- * @param {string} key
- * @returns {string}
- */
-function prettifyKey(key) {
-  let prettified = key.split('.')[1]
-  prettified = prettified.replace(/_/g, ' ')
-  return prettified.toUpperCase()
+		res.render('callback', {
+			data: [['sgID', sub], ...formattedData],
+		})
+	} catch (error) {
+		console.error(error)
+		res.status(500).render('error', { error })
+	}
 }
 
 module.exports = index
--- server.js
+++ server.js
@@ -2,18 +2,22 @@ require('dotenv').config()
 
 const PORT = process.env.PORT || 10000
 
+const express = require('express')
+const useragent = require('express-useragent')
+
+const auth = require('./routes/auth')
 const home = require('./scripts/home')
-const callback = require('./scripts/callback')
 const { csp } = require('./middlewares')
 
-const express = require('express')
 const app = express()
 
+app.use(useragent.express())
 app.use(csp())
+
+app.use(auth)
 app.use('/assets', express.static('assets'))
 app.set('view engine', 'ejs')
 
 app.get('/', home)
-app.get('/callback', callback)
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`))
