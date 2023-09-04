324221f3729379cf29dd149972b0d38ef1135727
Prawira Genestonlia <prawira.96@gmail.com>
2023-09-04 23:19:20 +0800

    feat: added passkey entry point

--- config/index.ts
+++ config/index.ts
@@ -2,6 +2,8 @@ export const BASE_URLS = {
   prod: 'https://api.id.gov.sg',
   stag: 'https://api-stg.id.gov.sg',
   dev: process.env.OVERRIDE_DEV,
+  passkeyProd: 'https://api.id.gov.sg',
+  passkeyStaging: 'https://api-stg.id.gov.sg',
 }
 export const PORT = process.env.PORT || 10000
 export const SCOPES = process.env.SCOPES || 'openid'
